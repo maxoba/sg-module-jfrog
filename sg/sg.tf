5b16cb844da94a129c620c08a7a8ad676863d447
your name <your email>
2026-04-04 23:47:01 -0600

    fix: sg module

---
+++ sg/sg.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "allow_tls" {
+  name        = var.sg_name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  /*
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+*/
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+provider "aws" {
+    region = var.region
+  
+}
