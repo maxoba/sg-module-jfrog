c6d9af16e07abc9bc35830bd1011d04a9302f2ad
your name <your email>
2026-04-10 02:39:10 -0600

    9

--- sg/sg/inputs.tf
+++ sg/sg/inputs.tf
@@ -1,12 +1,11 @@
 
 variable "ingress" {
   type    = list(number)
-  default = []
+
 }
 
 variable "egress" {
   type    = list(number)
-  default = [0] # can remove for provision by new
 }
 
 variable "sg_name" {
